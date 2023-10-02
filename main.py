b49f5c0cd7f39aff6d1ac1fb619a488066d42571
Seungyeoljeon <130431453+Seungyeoljeon@users.noreply.github.com>
2023-10-02 12:45:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,54 +27,70 @@ uploaded_file = streamlit.file_uploader("Choose a file")
 streamlit.write("___")
 
 def pdf_to_document(uploaded_file):
-    temp_dir = tempfile.TemporaryDirectory()
-    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
-    with open(temp_filepath,"wb") as f:
-        f.write(uploaded_file.getvalue())
-    loader = PyPDFLoader(temp_filepath)
-    pages = loader.load_and_split()
-    return pages
+    # 파일 확장자 확인
+    file_extension = os.path.splitext(uploaded_file.name)[1].lower()
+    
+    # PDF 파일의 경우
+    if file_extension == '.pdf':
+        temp_dir = tempfile.TemporaryDirectory()
+        temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+        with open(temp_filepath,"wb") as f:
+            f.write(uploaded_file.getvalue())
+        loader = PyPDFLoader(temp_filepath)
+        pages = loader.load_and_split()
+        return pages
+        
+    elif file_extension == '.txt':
+            # TXT 내용을 읽어서 리스트로 반환
+            content = uploaded_file.getvalue().decode('utf-8')
+            return [content]
+        else:
+            raise ValueError("Unsupported file type. Only PDF and TXT are supported.")
 
 
 #업로드시 동작 코드
 if uploaded_file is not None:
-    pages = pdf_to_document(uploaded_file)
+    try:
+        pages = pdf_to_document(uploaded_file)
+        # ... [나머지 코드]
+    except ValueError as e:
+        streamlit.error(str(e))
 
-    #Loader
-    # loader = PyPDFLoader("copyrightqa.pdf")
-    # pages = loader.load_and_split()
+#Loader
+# loader = PyPDFLoader("copyrightqa.pdf")
+# pages = loader.load_and_split()
+    
+#Split
+text_splitter = RecursiveCharacterTextSplitter(
+# Set a really small chunk size, just to show.
+    chunk_size = 300,
+    chunk_overlap  = 20,
+    length_function = len,
+    is_separator_regex = False,
+)
+texts = text_splitter.split_documents(pages)
 
-    #Split
-    text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-        chunk_size = 300,
-        chunk_overlap  = 20,
-        length_function = len,
-        is_separator_regex = False,
-    )
-    texts = text_splitter.split_documents(pages)
+#Embedding
 
-    #Embedding
+embeddings_model = OpenAIEmbeddings()
 
-    embeddings_model = OpenAIEmbeddings()
-
-    # load it into Chroma
-    db = Chroma.from_documents(texts, embeddings_model)
+# load it into Chroma
+db = Chroma.from_documents(texts, embeddings_model)
 
 
-    # llm = ChatOpenAI(temperature=0)
-    # retriever_from_llm = MultiQueryRetriever.from_llm(
-    #     retriever=db.as_retriever(), llm=llm
-    # )
+# llm = ChatOpenAI(temperature=0)
+# retriever_from_llm = MultiQueryRetriever.from_llm(
+#     retriever=db.as_retriever(), llm=llm
+# )
 
-    #input
-    streamlit.header("pdf에게 질문하기")
-    question = streamlit.text_input('질문입력하세요.')
+#input
+streamlit.header("pdf에게 질문하기")
+question = streamlit.text_input('질문입력하세요.')
 
-    #question
-    if streamlit.button('질문하기'):
-        with streamlit.spinner('Wait for it...'):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
-            result = qa_chain({"query": question})
-            streamlit.write(result["result"])
+#question
+if streamlit.button('질문하기'):
+    with streamlit.spinner('Wait for it...'):
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+        qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
+        result = qa_chain({"query": question})
+        streamlit.write(result["result"])
