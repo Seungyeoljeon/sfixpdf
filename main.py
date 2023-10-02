240cc5d788413a723bceba67bad90155fbc98fd7
Seungyeoljeon <130431453+Seungyeoljeon@users.noreply.github.com>
2023-10-02 14:24:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 #import
-from langchain.document_loaders import PyPDFLoader import TextLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
